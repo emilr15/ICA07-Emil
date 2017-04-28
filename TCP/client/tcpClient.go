15836a5d2b52e29631c7c594ec508ae36638a137
emilr15 <emilr15@uia.no>
2017-04-28 10:54:42 +0200

    endringar

--- TCP/client/tcpClient.go
+++ TCP/client/tcpClient.go
@@ -13,12 +13,12 @@ func main() {
 	for {
 		// read in input from stdin
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
+		fmt.Print("Tekst til server: ")
 		text, _ := reader.ReadString('\n')
 		// send to socket
 		fmt.Fprintf(conn, text+"\n")
 		// listen for reply
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
+		fmt.Print("Server: " + message)
 	}
 }
